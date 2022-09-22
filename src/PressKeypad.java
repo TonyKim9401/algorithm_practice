6dc8e4ddb3becac149e78049806a1d2eed6c9698
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2022-09-22 19:45:59 +0900

    Programmers level1 SEP.22.2022

--- src/PressKeypad.java
+++ src/PressKeypad.java
@@ -5,8 +5,60 @@ public class PressKeypad {
     static class Solution {
         public String solution(int[] numbers, String hand) {
 
-            // 개 망해서 다시 해야함.
-            return null;
+            String result = "";
+
+            int[] leftHand = new int[]{3,0};
+            int[] rightHand = new int[]{3,2};
+            int[] nowNode = new int[2];
+
+            for (int number : numbers) {
+                int leftNode = (number - 1) / 3;
+                int rightNode = (number - 1) % 3;
+                if(number == 0){
+                    leftNode = 3;
+                    rightNode = 1;
+                }
+
+                nowNode[0] = leftNode;
+                nowNode[1] = rightNode;
+
+                if(rightNode == 0){
+                    result += "L";
+                    leftHand[0] = leftNode;
+                    leftHand[1] = rightNode;
+                } else if(rightNode == 2){
+                    result += "R";
+                    rightHand[0] = leftNode;
+                    rightHand[1] = rightNode;
+                } else {
+                    int leftDist = Math.abs(leftHand[0] - nowNode[0]) + Math.abs(leftHand[1] - nowNode[1]);
+                    int rightDist = Math.abs(rightHand[0] - nowNode[0]) + Math.abs(rightHand[1] - nowNode[1]);
+
+                    if(leftDist > rightDist){
+                        result += "R";
+                        rightHand[0] = leftNode;
+                        rightHand[1] = rightNode;
+                    } else if(leftDist < rightDist){
+                        result += "L";
+                        leftHand[0] = leftNode;
+                        leftHand[1] = rightNode;
+                    } else {
+                        if(hand.equals("right")){
+                            result += "R";
+                            rightHand[0] = leftNode;
+                            rightHand[1] = rightNode;
+                        } else {
+                            result += "L";
+                            leftHand[0] = leftNode;
+                            leftHand[1] = rightNode;
+                        }
+                    }
+
+                }
+
+            }
+
+            return result;
         }
     }
 
