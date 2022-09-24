d90c3db7bf1a797f9be140f9776dfea00ca6ac2b
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2022-09-24 13:08:34 +0900

    Programmers level1 SEP.24.2022

---
+++ src/RecommendNewId.java
@@ -0,0 +1,17 @@
+public class RecommendNewId {
+    static class Solution {
+        public String solution(String new_id) {
+            String answer = "";
+
+            // 9월 25일 풀기
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.solution("...!@BaT#*..y.abcdefghijklm");
+        System.out.println(result);
+    }
+}
