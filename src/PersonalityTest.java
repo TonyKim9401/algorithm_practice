bf2115a072b53385059f5e716378339988463ec0
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2022-10-03 22:03:31 +0900

    Programmers level1 Oct.03.2022

---
+++ src/PersonalityTest.java
@@ -0,0 +1,17 @@
+public class PersonalityTest {
+    static class Solution {
+        public String solution(String[] survey, int[] choices) {
+            String answer = "";
+
+            // 10월 4일 이후에 풀것
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
+        System.out.println(result);
+    }
+}
