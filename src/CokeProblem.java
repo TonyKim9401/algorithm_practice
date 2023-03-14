c675546bafaa5c943f41d76b3838bdf6b4e5bc9e
companyLaptop <k.tony9412@gmail.com>
2023-03-14 16:01:44 +0900

    MAR.14.2023

--- src/CokeProblem.java
+++ src/CokeProblem.java
@@ -3,15 +3,10 @@ public class CokeProblem {
         public int solution(int a, int b, int n) {
             int answer = 0;
 
-            // 실패, 수정 필요 2022.10.21
-            int rcvCoke = 0;
 
-            while(a <= n) {
-
-                rcvCoke = n / a;
-                n = n - (rcvCoke * a) + rcvCoke;
-                answer += rcvCoke;
-
+            while( a <= n ) {
+                n = n - a + b;
+                answer += b;
             }
 
             return answer;
@@ -21,9 +16,10 @@ public class CokeProblem {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int result = solution.solution(2, 1, 20);
-//        int result = solution.solution(3, 1, 20);
+        int result2 = solution.solution(3, 1, 20);
 
         System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
 
     }
 }
