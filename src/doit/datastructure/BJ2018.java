2cf3c044449b2fc27a481b574d6aced0841abbb3
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-06-16 23:53:12 +0900

    JUNE.16.2023
    BJ2018

--- src/doit/datastructure/BJ2018.java
+++ src/doit/datastructure/BJ2018.java
@@ -13,14 +13,32 @@ public class BJ2018 {
         int sum = 1;
 
         while (end_index != N) {
+            /**
+             * N과 투포인트의 합이 같으면
+             * 카운트 + 1
+             * 끝점 + 1
+             * 합 = 합 + 끝점 인덱스값
+             */
             if (sum == N) {
                 count++;
                 end_index++;
                 sum += end_index;
-            } else if (sum > N) {
+            }
+            /**
+             * sum이 N보다 크면
+             * sum = sum - 시작 인덱스 값
+             * 시작 인덱스 + 1
+             */
+            else if (sum > N) {
                 sum -= start_index;
                 start_index++;
-            } else if (sum < N) {
+            }
+            /**
+             * sun이 N보다 작으면
+             * 끝점 + 1
+             * sum = sum + 끝점
+             */
+            else if (sum < N) {
                 end_index++;
                 sum += end_index;
             }
