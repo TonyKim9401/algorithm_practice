b6a1aba3f605066433951c8a83cae509f88ec8e8
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-03 23:32:46 +0900

    JULY.03.2023
    명예의전당 (1) - PriorityQueue

--- src/programmers/TheHallOfFrame.java
+++ src/programmers/TheHallOfFrame.java
@@ -7,15 +7,16 @@ public class TheHallOfFrame {
     static class Solution {
         public int[] solution(int k, int[] score) {
             int[] answer = new int[score.length];
-            List<Integer> temp = new ArrayList<>();
+
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
 
             for(int i = 0; i < score.length; i++) {
-                temp.add(score[i]);
-                temp.sort(Comparator.comparingInt(o -> o));
-                if (i >= k) temp.remove(0);
-                answer[i] = temp.get(0);
+                queue.add(score[i]);
+                if (queue.size() > k) {
+                    queue.poll();
+                }
+                answer[i] = queue.peek();
             }
-
             return answer;
         }
     }
