415bad677829005b3938d1451f44838e804d6181
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-08 18:31:42 +0900

    JULY.08.2023
    옹알이(2)

---
+++ src/programmers/Babbling.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class Babbling {
+
+    static class Solution {
+        public int solution(String[] babbling) {
+            int answer = 0;
+
+            // reaplseAll 정규식에 대해 기억해 둘것
+            for(String target : babbling) {
+                target = target.replaceAll("ayaaya|yeye|mama|woowoo", " ");
+                target = target.replaceAll("aya|ye|ma|woo", "");
+                if(target.equals("")) answer++;
+            }
+
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int result = solution.solution(new String[]{"yayae","ayaye","woowoo", "uuu", "yeye", "yemawoo", "ayaayaa"});
+        System.out.println("result = " + result);
+
+    }
+}
