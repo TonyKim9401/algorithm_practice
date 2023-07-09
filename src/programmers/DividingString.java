9e609403d1cf985d6af07e683e2199f0e3060a02
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-09 18:57:20 +0900

    JULY.09.2023
    문자열 나누기

---
+++ src/programmers/DividingString.java
@@ -0,0 +1,42 @@
+package programmers;
+
+public class DividingString {
+    static class Solution {
+        public int solution(String s) {
+            int answer = 0;
+
+            String x = String.valueOf(s.charAt(0));
+            int[] comp = new int[2];
+
+            for(int i = 0; i < s.length(); i++){
+
+                if(x == null) {
+                    x = String.valueOf(s.charAt(i));
+                }
+                if(x.equals(String.valueOf(s.charAt(i)))) {
+                    comp[0]++;
+                } else {
+                    comp[1]++;
+                }
+                if(comp[0] == comp[1]){
+                    answer++;
+                    comp[0] = 0;
+                    comp[1] = 0;
+                    x = null;
+                }
+            }
+
+            if(comp[0] != 0 || comp[1] !=0 ){
+                answer++;
+            }
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution("abracadabra");
+        System.out.println("result = " + result);
+    }
+}
