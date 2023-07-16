4bfaa24545ae95928e7579049a4033a38f3e631b
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-16 15:25:56 +0900

    JULY.16.2023
    바탕화면 정리하기

---
+++ src/programmers/CleanWallpaper.java
@@ -0,0 +1,43 @@
+package programmers;
+
+public class CleanWallpaper {
+    static class Solution {
+        public int[] solution(String[] wallpaper) {
+
+            int[][] check = new int[wallpaper.length][wallpaper[0].length()];
+
+            int xMin = wallpaper[0].length();    //x 최소
+            int yMin = wallpaper.length;;  // y 최소
+            int xMax = 0; // x 최대
+            int yMax = 0;   //y 최대
+
+            int curX = 0;
+            int curY = 0;
+
+            for(int i = 0; i < wallpaper.length; i++){
+                String x = wallpaper[i];
+                for(int j = 0; j < x.length(); j++) {
+                    curX = j;
+                    curY = i;
+                    char value = x.charAt(j);
+                    if (value == '#') {
+                        if (xMin > curX) xMin = curX;
+                        if (yMin > curY) yMin = curY;
+                        if (xMax < curX) xMax = curX;
+                        if (yMax < curY) yMax = curY;
+                    }
+                }
+            }
+            return new int[]{yMin, xMin ,yMax+1, xMax + 1};
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.solution(new String[]{"..", "#."});
+//        int[] result = solution.solution(new String[]{".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."});
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
