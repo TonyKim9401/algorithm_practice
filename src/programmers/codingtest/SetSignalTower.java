2a158e26bb77fc75895ff54b0e7485aaa927fc3c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-20 01:15:59 +0900

    JULY.19.2023
    프로그래머스 코테
    1일차

--- src/programmers/codingtest/SetSignalTower.java
+++ src/programmers/codingtest/SetSignalTower.java
@@ -5,36 +5,20 @@ public class SetSignalTower {
         public int solution(int n, int[] stations, int w) {
             int answer = 0;
 
-            int startIdx = 1;
-            int fullCover = (w * 2) + 1;
+            int station = 0;
+            int idx = 1;
 
-            for(int i = 0; i < stations.length; i++) {
-                int startCover = stations[i] - w <= 1 ? 1 : stations[i] - w;
-                int endCover = stations[i] + w >= n ? n : stations[i] + w;
-
-                if (startCover <= startIdx) {
-                    startIdx = endCover + 1;
-                    continue;
+            while(idx <= n) { // < 로 하면 맨 마지막에 남았을때 해당이 안됨
+                //기존 기지국 범위                 인덱스가 해당 기지국 맨 앞에보다 크거나 같으면
+                if (station < stations.length && idx >= stations[station] - w) {
+                    idx = stations[station] + w + 1; //인덱스를 기지국 맨 뒤 + 1로 옮겨줌
+                    station++;
+                } else {
+                    //새로 설치한 기지국 범위
+                    answer++;
+                    idx += (w * 2) + 1; //양쪽 영향 + 기지국 1칸
                 }
-
-                int range = startCover - startIdx;
-                int x = range / fullCover;
-                int y = range % fullCover;
-                answer += x;
-                if(y > 0) answer++;
-
-                startIdx = endCover + 1; // 다음으로
             }
-
-            if (startIdx < n) {
-                int startCover = startIdx;
-                int range = n - startCover;
-                int x = range / fullCover;
-                int y = range % fullCover;
-                answer += x;
-                if(y > 0) answer++;
-            }
-
 
             return answer;
         }
@@ -43,7 +27,7 @@ public class SetSignalTower {
     public static void main(String[] args) {
         Solution solution = new Solution();
 //        int result = solution.solution(16, new int[]{9}, 2);
-        int result = solution.solution(11, new int[]{4, 11}, 1);
+        int result = solution.solution(11, new int[]{2, 11}, 1);
         System.out.println("result = " + result);
     }
 }
