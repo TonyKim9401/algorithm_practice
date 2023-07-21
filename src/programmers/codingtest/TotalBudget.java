e05add3afb772949b453313ce6e21a60b6b2906d
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-21 19:54:42 +0900

    JULY.21.2023
    프로그래머스 코테
    3일차

--- src/programmers/codingtest/TotalBudget.java
+++ src/programmers/codingtest/TotalBudget.java
@@ -6,74 +6,32 @@ public class TotalBudget {
 
     static class Solution {
         public int solution(int[] budgets, int M) {
+            int answer= 0 ;
 
-            int answer = 0;
-
-            int average = M / budgets.length;
-            int rest = M % budgets.length;
-
-            // 모든 요청 배정 가능
-            if (rest == 0) {
-                return average;
+            int min = 0;
+            int max = 0;
+            for (int b : budgets) {
+                if (b > max) max = b;
             }
 
-            // 평균보다 작은 예산만 담고 어디까지 인지 확인
-            List<Integer> smallList = new ArrayList<>();
-            int smallSize = 0;
-            for (int i = 0; i < budgets.length; i++) {
-                if (budgets[i] <= average) {
-                    smallSize += 1;
-                    smallList.add(budgets[i]);
-                    continue;
+            while (min < max) {
+                int mid = (min + max) / 2;
+                int sum = 0;
+
+                for (int b : budgets) {
+                    if (b > mid) sum += mid;
+                    else sum += b;
                 }
-                break;
-            }
-            int averageTotal = Arrays.stream(budgets).sum() / budgets.length;
-            int restSize = budgets.length - smallSize;
-            int restBudget = M - smallList.stream().mapToInt(s -> s).sum();
 
-            for (int i = average; i <= averageTotal; i++) {
-                if (i * restSize > restBudget) {
-                    answer = i - 1;
-                    break;
+                if (sum <= M) {
+                    min = mid + 1;
+                    answer = mid;
+                } else {
+                    max = mid - 1;
                 }
             }
 
             return answer;
-
-            /*
-            int answer = 0;
-
-            int average = M / budgets.length;
-            int rest = M % budgets.length;
-
-            // 모든 요청 배정 가능
-            if (rest == 0) {
-                return average;
-            }
-
-            // 평균보다 작은 예산만 담고 어디까지 인지 확인
-            List<Integer> smallList = new ArrayList<>();
-            int smallSize = 0;
-            for (int i = 0; i < budgets.length; i++) {
-                if (budgets[i] <= average) {
-                    smallSize += 1;
-                    smallList.add(budgets[i]);
-                    continue;
-                }
-                break;
-            }
-
-            // 평균보다 작은 예산의 전체합
-            int smallTotal = smallList.stream().mapToInt(s -> s).sum();
-
-            // 전체 예산 - 작은 예산의 합
-            int restBudget = M - smallTotal;
-            // 전체 지방수 - 작은 예산 지방 수
-            int restSize = budgets.length - smallSize;
-
-            return restBudget / restSize;
-            */
         }
     }
 
