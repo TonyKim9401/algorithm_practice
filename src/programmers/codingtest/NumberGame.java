3b703b3f06cd5b37d3407e21e5438dd2deaa7a57
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-22 19:33:57 +0900

    JULY.22.2023
    프로그래머스 코테
    4일차

---
+++ src/programmers/codingtest/NumberGame.java
@@ -0,0 +1,41 @@
+package programmers.codingtest;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class NumberGame {
+    static class Solution {
+        public int solution(int[] A, int[] B) {
+            int answer = 0;
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            List<Integer> list = new ArrayList<>();
+            for(int num : B) {
+                list.add(num);
+            }
+
+            int idx = 0;
+            for(int i = 0; i < list.size(); i++) {
+                if(A[idx] < list.get(i)) {
+                    answer += 1;
+                    idx += 1;
+                    list.remove(i);
+                    i -= 1;
+                }
+            }
+
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(new int[]{5,1,3,7}, new int[]{2,2,6,8});
+        System.out.println("result = " + result);
+    }
+
+
+}
