48c5284bcd6c9e3fb539240576c6a97a405f2dde
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-22 19:33:49 +0900

    JULY.22.2023
    프로그래머스 코테
    4일차

---
+++ src/ndb/FindParts.java
@@ -0,0 +1,54 @@
+package ndb;
+
+import java.io.*;
+import java.util.*;
+
+public class FindParts {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] store = new int[N];
+        for(int i = 0; i < N; i++) {
+            store[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // store 물건 한번 정렬
+        Arrays.sort(store);
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] customer = new int[M];
+        for(int i = 0; i < M; i++) {
+            customer[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < customer.length; i++) {
+            int num = customer[i];
+            int start = 0;
+            int end = store.length - 1;
+            String answer = "No";
+
+            while (start <= end) {
+
+                int mid = (start + end) / 2;
+
+                if (store[mid] == num) {
+                    answer = "Yes";
+                    break;
+                } else if (store[mid] < num) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            sb.append(answer.equals("Yes") ? "Yes\n" : "No\n");
+        }
+        System.out.println(sb.toString());
+
+    }
+}
