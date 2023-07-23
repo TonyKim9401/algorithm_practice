86c9bf1359cade50f6900a95aa6734493a47ae8c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-24 00:55:15 +0900

    JULY.24.2023
    프로그래머스 코테
    6일차

---
+++ src/ndb/MulOrPlus.java
@@ -0,0 +1,22 @@
+package ndb;
+
+public class MulOrPlus {
+    public static void main(String[] args) {
+        int answer = 0 ;
+        String num = "02078";
+
+
+        for(int i = 0; i < num.length(); i++) {
+            int check = num.charAt(i) - '0';
+            if (check <= 1 || answer <= 1) {
+                answer += check;
+                continue;
+            }
+            answer *= check;
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
