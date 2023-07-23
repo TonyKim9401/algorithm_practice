7abc145f2ea48933033a6ded16d4b20b2836a58e
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-24 00:00:04 +0900

    JULY.23.2023
    프로그래머스 코테
    5일차

---
+++ src/ndb/Changes.java
@@ -0,0 +1,20 @@
+package ndb;
+
+public class Changes {
+    public static void main(String[] args) {
+
+        int answer = 0;
+        int N = 1260;
+
+        int[] changes = new int[]{500, 100, 50, 10};
+
+        for (int change : changes) {
+
+            answer += N / change;
+            N = N % change;
+
+        }
+        System.out.println(answer);
+
+    }
+}
