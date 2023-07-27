dd17f41614b95a12cc215b19eb8970c10ac5ace7
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-27 15:26:19 +0900

    JULY.27.2023
    프로그래머스 코테
    9일차

--- src/ndb/sort/HowManyX.java
+++ src/ndb/sort/HowManyX.java
@@ -4,15 +4,11 @@ import java.util.stream.IntStream;
 
 public class HowManyX {
     public static void main(String[] args) {
-
         int x = 2;
-
         int[] arr = {1,1,2,2,2,2,3};
 
         int min = 0;
         int max = arr.length - 1;
-
-        int left = 0;
 
         while (min < max) {
             int mid = (min + max) / 2;
@@ -20,11 +16,9 @@ public class HowManyX {
             else min = mid + 1;
         }
 
-
         int min2 = 0;
         int max2 = arr.length - 1;
 
-        int right = 0;
         while (min2 < max2) {
             int mid = (min2 + max2) / 2;
             if (arr[mid] > x) max2 = mid;
