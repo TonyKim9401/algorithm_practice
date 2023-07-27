711b71c1f145432a04caa0818054c1cfc3b11ba4
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-27 17:38:43 +0900

    JULY.27.2023
    프로그래머스 코테
    9일차

--- src/ndb/greedy/AdventureGuild.java
+++ src/ndb/greedy/AdventureGuild.java
@@ -2,30 +2,32 @@ package ndb.greedy;
 
 import java.util.*;
 import java.util.stream.*;
+import java.io.*;
 
 public class AdventureGuild {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
-        int N = 5;
-        int[] players = {2,3,1,2,2};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] players = new int[N];
+        int count = 0;
+        int answer = 0;
+
+        for (int i = 0; i <N; i++) {
+            players[i] = Integer.parseInt(st.nextToken());
+        }
 
         Arrays.sort(players);
-        int answer = 0;
-        int count = 0;
 
-        //[1] [2 2] 2 3
-        for (int i = 0; i < players.length; i++) {
-            count += 1;
-
-            if (count >= players[i]) {
+        for (int player : players) {
+            count += 1; // for 문 횟수 만큼의 총 사람 수
+            if (count >= player) { // 구획 나누기
                 answer += 1;
                 count = 0;
             }
-
         }
 
-        System.out.println(count);
-
-
+        System.out.println(answer);
     }
 }
