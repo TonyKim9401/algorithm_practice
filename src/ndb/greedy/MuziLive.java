849fc54a8153a3867f4c21bf968c7d60d7bb2e2e
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-28 15:11:50 +0900

    JULY.28.2023
    프로그래머스 코테
    10일차

--- src/ndb/greedy/MuziLive.java
+++ src/ndb/greedy/MuziLive.java
@@ -1,43 +1,61 @@
 package ndb.greedy;
 
+import java.util.*;
+
 public class MuziLive {
 
+    static class Food {
+        int time;
+        int idx;
+        Food(int t, int i) {
+            this.time = t;
+            this.idx = i;
+        }
+    }
     static class Solution {
         public int solution(int[] food_times, long k) {
 
-            /**
-             * 효율성 망함, 정답도 맞지는 않음
-             */
-            int idx = 0;
-            int end = food_times.length;
-            int zeroCount = 0;
-
-            for (long i = 0; i < k; i++) {
-
-                if (food_times[idx] != 0) {
-                    food_times[idx] -= 1;
-                    if (food_times[idx] == 0) zeroCount += 1;
-                    idx += 1;
-                } else {
-                    idx += 1;
-                    i -= 1;
-                }
-
-                if (zeroCount == end) return -1;
-                if (idx == end) idx = 0;
+            List<Food> foods = new ArrayList<>();
+            int n = food_times.length;
+            for (int i = 0; i < food_times.length; i++) {
+                Food food = new Food(food_times[i], i + 1);
+                foods.add(food);
             }
 
-            if (idx + 1 == end) return 0;
+            foods.sort((o1, o2) -> {
+                return o1.time - o2.time;
+            });
 
-            return idx + 1;
+            int pretime = 0;
+            int i = 0;
 
+            for (Food f : foods) {
+                long diff = f.time - pretime;
+                if (diff != 0) {
+                    long spend = diff * n; // 1 * 6
+                    if (spend <= k) {
+                        k -= spend;
+                        pretime = f.time; // 1
+                    } else {
+                        k %= n;
+                        foods.subList(i, food_times.length).sort((o1, o2) ->{
+                           return o1.idx - o2.idx;
+                        });
+                        return foods.get(i + (int)k).idx;
+                    }
+                }
+                i += 1;
+                n -= 1;
+            }
+
+            return -1;
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int result = solution.solution(new int[]{3, 1, 2}, 5l);
-        int result = solution.solution(new int[]{3, 3, 3}, 8l);
+        int result = solution.solution(new int[]{3, 5, 1, 6, 5, 3}, 20l);
+//        int result = solution.solution(new int[]{3, 3, 3}, 8l);
         System.out.println(result);
     }
 
