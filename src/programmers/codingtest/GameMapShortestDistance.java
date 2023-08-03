4db0409f52f5d84d2ea9118cbf6253c0b27f7a22
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-04 01:40:48 +0900

    AUG.04.2023
    프로그래머스 코테
    17일차

--- src/programmers/codingtest/GameMapShortestDistance.java
+++ src/programmers/codingtest/GameMapShortestDistance.java
@@ -11,9 +11,17 @@ public class GameMapShortestDistance {
             this.y = y;
         }
     }
+    static class Location {
+        int x;
+        int y;
+        Location(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static class Solution {
         public int solution(int[][] maps) {
-            int answer = 0;
+            /*int answer = 0;
 
             int xMax = maps[0].length;
             int yMax = maps.length;
@@ -52,7 +60,47 @@ public class GameMapShortestDistance {
             }
 
             answer = passed[yMax - 1][xMax - 1];
-            return answer == 0 ? -1 : answer;
+            return answer == 0 ? -1 : answer;*/
+
+            int xMax = maps[0].length;
+            int yMax = maps.length;
+
+            int[][] direction = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
+            int[][] stepCount = new int[yMax][xMax];
+            boolean[][] validate = new boolean[yMax][xMax];
+
+            stepCount[0][0] = 1;
+            validate[0][0] = true;
+
+            Queue<Location> q = new LinkedList<>();
+            q.offer(new Location(0, 0));
+
+            while (!q.isEmpty()) {
+                Location loc = q.poll();
+                int x = loc.x;
+                int y = loc.y;
+                int curStep = stepCount[y][x];
+                for (int i = 0; i < 4; i++) {
+                    int moveX = x + direction[i][1];
+                    int moveY = y + direction[i][0];
+                /*
+                1.전체 크기에서 벗어나지 않아야함
+                2.벽에 부딪히지 않아야 함
+                3.이미 지나간 길은 돌아갈 수 없음
+                */
+                    if (moveX < 0 || moveX >= xMax || moveY < 0 || moveY >= yMax) continue;
+                    if (maps[moveX][moveY] == 1) continue;
+                    if (validate[moveY][moveX]) continue;
+                    stepCount[moveY][moveX] = curStep + 1;
+                    validate[moveY][moveX] = true;
+                    q.offer(new Location(moveX, moveY));
+                }
+            }
+
+            int totalStep = stepCount[yMax-1][xMax-1];
+
+            return totalStep == 0 ? -1 : totalStep;
         }
     }
 
