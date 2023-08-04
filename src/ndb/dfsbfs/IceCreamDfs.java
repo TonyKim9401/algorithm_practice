dde3bf81281e83787920a9376d2aa5cecc20337a
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-04 17:56:47 +0900

    AUG.04.2023
    프로그래머스 코테
    17일차

---
+++ src/ndb/dfsbfs/IceCreamDfs.java
@@ -0,0 +1,53 @@
+package ndb.dfsbfs;
+
+import java.util.*;
+
+public class IceCreamDfs {
+
+
+    public static int n = 4; // y
+    public static int m = 5; // x
+
+    public static int[][] graph = new int[][]{
+            {0,0,1,1,0},
+            {0,0,0,1,0},
+            {1,1,1,1,0},
+            {0,0,0,0,0}
+    };
+
+    // DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+    public static boolean dfs(int x, int y) {
+        // 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if (x <= -1 || x >=n || y <= -1 || y >= m) {
+            return false;
+        }
+        // 현재 노드를 아직 방문하지 않았다면
+        if (graph[x][y] == 0) {
+            // 해당 노드 방문 처리
+            graph[x][y] = 1;
+            // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // 현재 위치에서 DFS 수행
+                if (dfs(i, j)) {
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result); // 정답 출력
+    }
+
+}
