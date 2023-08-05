1365cb23dc961ec3930eade10eb7bdb335532e9c
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-06 00:33:53 +0900

    AUG.06.2023
    프로그래머스 코테
    19일차

--- src/ndb/dfsbfs/CitiFind.java
+++ src/ndb/dfsbfs/CitiFind.java
@@ -1,10 +1,74 @@
 package ndb.dfsbfs;
 
+import java.sql.SQLOutput;
 import java.util.*;
+import java.io.*;
 
 public class CitiFind {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        // 구간 합
+        int[] values = new int[3000001];
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            graph.add(new ArrayList<>());
+            values[i] = -1;
+        }
+
+        // 그래프 채우기
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            graph.get(Integer.parseInt(st.nextToken())).add(Integer.parseInt(st.nextToken()));
+        }
+
+
+        Queue<Integer> q = new LinkedList<>();
+        values[X] = 0; // 초기화
+        q.offer(X);
+
+        while (!q.isEmpty()) {
+            int city = q.poll();
+            List<Integer> nextCity = graph.get(city);
+            for (int i = 0; i < nextCity.size(); i++) {
+                if (values[nextCity.get(i)] == -1) values[nextCity.get(i)] = values[city] + 1;
+                q.offer(nextCity.get(i));
+            }
+
+        }
+
+        boolean check = false;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != -1 && values[i] == K) {
+                result.add(i);
+                check = true;
+            }
+        }
+
+        if (!check) {
+            System.out.println(-1);
+        }
+        Collections.sort(result);
+        for (Integer i : result) {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+
+
+
+        /*
         int n = 4;
         int x = 1;
         int k = 2;
@@ -46,7 +110,4 @@ public class CitiFind {
             }
         }
 
-        if (!check) System.out.println(-1);
-
-    }
-}
+        if (!check) System.out.println(-1);*/
