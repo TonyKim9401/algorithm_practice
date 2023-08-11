febaa122e145f5c4526dfc93c04899c19c4cd015
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-11 20:08:33 +0900

    AUG.11.2023
    프로그래머스 코테
    24일차

--- src/programmers/WordPuzzle.java
+++ src/programmers/WordPuzzle.java
@@ -5,27 +5,32 @@ import java.util.*;
 public class WordPuzzle {
     static class Solution {
         public int solution(String[] strs, String t) {
-            int answer = Integer.MAX_VALUE;
+            int answer = 0;
 
-            int[] dp = new int[t.length()];
+            int strsLength = strs.length;
+            int[] dp = new int[t.length() + 1];
 
             for (int i = 1; i < t.length() + 1; i++) {
-                for (int j = 0; j < strs.length; j++) {
-                    String puzzle = strs[j];
-                    if (i - puzzle.length() < 0) continue;
-                    if (puzzle.equals(t.substring(i - puzzle.length(), i))) {
-                        if (i - puzzle.length() == 0) {
-                            dp[i - 1] = 1;
+                for (int j = 0; j < strsLength; j++) {
+
+                    String word = strs[j];
+                    int wordLength = word.length();
+                    if (i - wordLength < 0) continue;
+
+                    if (word.equals(t.substring(i - wordLength, i))) {
+                        if (i - wordLength == 0) {
+                            dp[i] = 1;
                             continue;
                         }
-                        if (dp[i - puzzle.length() - 1] > 0) {
-                            dp[i - 1] = dp[i - 1] == 0? dp[i - puzzle.length() - 1] + 1 :
-                                    Math.min(dp[i - 1], dp[i - puzzle.length() - 1] + 1);
+                        if (dp[i - wordLength] > 0) {
+                            dp[i] = dp[i] == 0 ? dp[i - wordLength] + 1 :
+                                    Math.min(dp[i], dp[i - wordLength] + 1);
                         }
                     }
                 }
             }
-            answer = dp[dp.length - 1];
+
+            answer = dp[t.length()];
             if (answer == 0) return -1;
             return answer;
         }
@@ -34,8 +39,8 @@ public class WordPuzzle {
     public static void main(String[] args) {
         Solution solution = new Solution();
 //        int result = solution.solution(new String[]{"ba","na","n","a"}, "banana");
-//        int result = solution.solution(new String[]{"app","ap","p","l","e","ple","pp"}, "apple");
-        int result = solution.solution(new String[]{"ba","an","nan","ban","n"}, "banana");
+        int result = solution.solution(new String[]{"app","ap","p","l","e","ple","pp"}, "apple");
+//        int result = solution.solution(new String[]{"ba","an","nan","ban","n"}, "banana");
         System.out.println(result);
     }
 }
