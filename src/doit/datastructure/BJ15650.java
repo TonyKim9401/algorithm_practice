18d305d6288d3eee412bd416a87672fd78ca73dc
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-08-14 23:53:28 +0900

    AUG.14.2023
    프로그래머스 코테
    27일차

---
+++ src/doit/datastructure/BJ15650.java
@@ -0,0 +1,42 @@
+package doit.datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15650 {
+    public static boolean[] visit;
+    public static int[] arr;
+    public static int N, M;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[N];
+        arr = new int[M];
+
+        dfs(1,0);
+    }
+
+    public static void dfs(int at, int depth) {
+
+        if (depth == M) {
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
